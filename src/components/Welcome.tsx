2cf67d50833f6ee43c231971408ea45a9a3e70bc
Grace <gracelo0717@gmail.com>
2025-03-18 15:52:31 -0400

    basic components

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Welcome = () => {
+  return (
+    <div className='welcome'>
+      <h1>Go Touch Some Grass!</h1>
+      <p>
+        Answer a few simple questions to find out your next travel destination
+        suggested by OpenAI.
+      </p>
+      <button>Get Started</button>
+    </div>
+  );
+};
+
+export default Welcome;
