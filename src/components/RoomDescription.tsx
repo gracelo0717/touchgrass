a8b0f97380ea900a275d332b262883c1228fe539
Grace <gracelo0717@gmail.com>
2025-03-21 21:48:54 -0400

    update roomdescription

--- src/components/RoomDescription.tsx
+++ src/components/RoomDescription.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+interface Location {
+  description: string;
+}
 
 const RoomDescription = () => {
-  return <div>RoomDescription</div>;
+  const location = useLocation();
+  const { description } = location.state || { description: '' };
+
+  console.log('Room Description State:', description);
+
+  return (
+    <div className='room-description'>
+      <div className='room-description-content'>
+        <h3>Room Description</h3>
+        <p>{description ? description : 'No description available'}</p>
+      </div>
+    </div>
+  );
 };
 
 export default RoomDescription;
