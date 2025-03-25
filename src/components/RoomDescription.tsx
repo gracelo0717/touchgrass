69c8d53316654dd60fa796cbaa13ca232095fadb
Grace <gracelo0717@gmail.com>
2025-03-25 14:59:44 -0400

    update roomDescription

--- src/components/RoomDescription.tsx
+++ src/components/RoomDescription.tsx
@@ -1,21 +1,31 @@
-import React from 'react';
 import { useLocation } from 'react-router-dom';
-
-interface Location {
-  description: string;
-}
 
 const RoomDescription = () => {
   const location = useLocation();
-  const { description } = location.state || { description: '' };
+  const { description, objects } = location.state || {};
 
-  console.log('Room Description State:', description);
+  const handleOnClick = (object: string) => {
+    console.log(`Inspect this ${object}`);
+  };
 
   return (
     <div className='room-description'>
-      <div className='room-description-content'>
-        <h3>Room Description</h3>
-        <p>{description ? description : 'No description available'}</p>
+      <h3>Room Description</h3>
+      <p>{description}</p>
+
+      <div className='objects'>
+        <h4>Objects to Inspect:</h4>
+        <div className='button-container'>
+          {objects && objects.length > 0 ? (
+            objects.map((object: string, index: number) => (
+              <button key={index} onClick={() => handleOnClick(object)}>
+                Inspect {object}
+              </button>
+            ))
+          ) : (
+            <p>No objects available to inspect.</p>
+          )}
+        </div>
       </div>
     </div>
   );
