ae102be05c95b32550320a4de430a0658450e856
Grace <gracelo0717@gmail.com>
2025-03-27 16:06:50 -0400

    basic setup for object riddle

--- src/components/ObjectInspection.tsx
+++ src/components/ObjectInspection.tsx
@@ -1,11 +1,38 @@
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 const ObjectInspection = () => {
   const { object } = useParams();
 
+  const [riddle, setRiddle] = useState('');
+  const [answer, setAnswer] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setRiddle('Input Riddle about Object or Room...');
+      setLoading(false);
+    }, 1000);
+  }, [object]);
+
   return (
     <div className='inspection'>
       <h4>Inspecting {object}</h4>
+
+      {loading ? (
+        <p>Loading riddle...</p>
+      ) : (
+        <>
+          <p>{riddle}</p>
+          <input
+            type='text'
+            value={answer}
+            onChange={(e) => setAnswer(e.target.value)}
+            placeholder='Enter your Answer'
+          />
+        </>
+      )}
     </div>
   );
 };
