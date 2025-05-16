be25d48d3d5d20daf652486f9f05e5f89a288ff3
Grace <gracelo0717@gmail.com>
2025-05-16 14:47:52 -0400

    endGame

--- src/components/EndGame.tsx
+++ src/components/EndGame.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const EndGame = () => {
-  return <div>EndGame</div>;
+  return (
+    <div>
+      <h1>You Escaped!</h1>
+      <p>Congratulations, you completed all rooms and escaped the house!</p>
+      <button
+        onClick={() => {
+          localStorage.clear();
+          window.location.href = '/';
+        }}
+      >
+        Play Again
+      </button>
+    </div>
+  );
 };
 
 export default EndGame;
